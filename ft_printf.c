ebdfb1e84f04774920dbdb0bc44169193f303790
Lorenzo Yanga <lorenzoyanga@gmail.com>
2025-05-21 21:54:55 +0800

    incomplete work

--- ft_printf.c
+++ ft_printf.c
@@ -6,23 +6,90 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 18:25:10 by lyanga            #+#    #+#             */
-/*   Updated: 2025/05/16 19:14:56 by lyanga           ###   ########.fr       */
+/*   Updated: 2025/05/21 21:51:58 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <stdio.h>
+#include "ft_printf.h"
 
-void	ft_printf(char *format, ...)
+int	vars_handlewidth(char *str, t_vars *vars)
 {
-	// what lol
-	// grab the variables 
-	va_list arg;
-	
-	va_start (arg, format);
-	vfprintf (stdout, format, arg);
-	va_end (arg);
+			// get atoi of *str
+			// overwrite var->width
+			// get digits of the result of atoi
+			// increment str by digits (leave 1 for the default ++)
+}
 
+int vars_handleprecision(char *str, t_vars *vars)
+{
+	vars->flag |= flag_period;
+	// get atoi of *chr
+	// overwrite var->precision
+	// get digits of the result of atoi
+	// increment str by digits
+}
+
+int vars_create(char *str, const char *set, const char *end, t_vars *vars)
+{
+	char *chr;
+	char *str;
+	while (ft_strchr(end, str) == NULL)
+	{
+		chr = ft_strchr(set, str);
+		if (*chr == '-')
+			vars->flag |= flag_dash;
+		else if (ft_isnum(*chr) && *chr != '0')
+		{
+			// get atoi of *str
+			// overwrite var->width
+			// get digits of the result of atoi
+			// increment str by digits (leave 1 for the default ++)
+		}
+		else if (*chr == '0')
+			vars->flag |= flag_zero;
+		else if (*chr == '.')
+		{
+			vars->flag |= flag_period;
+			// get atoi of *chr
+			// overwrite var->precision
+			// get digits of the result of atoi
+			// increment str by digits
+		}
+		else if (*chr == '#')
+			vars->flag |= flag_pound;
+		else if (*chr == ' ')
+			vars->flag |= flag_space;
+		else if (*chr == '+')
+			vars->flag |= flag_plus;
+		str++;
+	}
+	var->conversion = ft_strchr(end, str);
+}
+
+void	ft_printf(char *str, ...)
+{
+	va_list args;
+	const char	*set = (const)ft_strdup("-0.# +123456789");
+	const char	*end = (const)ft_strdup("cspdiuxX%");
+	t_vars	*result;
+
+	result = malloc(sizeof(t_vars));
+	if (!result)
+		return (NULL);
+	
+	va_start (args, str);
+	while (*str != '\0')
+	{
+		if (*str == '%')
+		{
+			flags = 
+			str++;
+		}
+		
+	}
+	va_end (args);
+	free(set);
+	free(end);
 }
 
 int main(void)
--- ft_printf.h
+++ ft_printf.h
@@ -6,13 +6,46 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 18:26:30 by lyanga            #+#    #+#             */
-/*   Updated: 2025/05/16 18:54:23 by lyanga           ###   ########.fr       */
+/*   Updated: 2025/05/21 21:50:35 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <stdargs.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "libft/libft.h"
+
+enum e_flagtypes {
+	flag_none	= 0b000000,
+	flag_dash	= 0b000001,
+	flag_zero	= 0b000010,
+	flag_period	= 0b000100,
+	flag_pound	= 0b001000,
+	flag_space	= 0b010000,
+	flag_plus	= 0b100000
+}
+
+enum e_conversions{
+	conv_nil	= NULL,
+	conv_c		= 'c', // char
+	conv_s		= 's', // string
+	conv_p		= 'p', // ptr address
+	conv_d		= 'd', // decimal base10
+	conv_i		= 'i', // int base 10 (same as d)
+	conv_u		= 'u', // unsigned base 10
+	conv_x		= 'x', // hexa lowercase
+	conv_X 		= 'X', // hexa uppercase
+	conv_percent = '%' // escape to render %
+}
+
+typedef struct s_vars {
+	int	flag;
+	size_t width;
+	size_t precision;
+	char conversion;
+} t_vars;
 
 #endif
